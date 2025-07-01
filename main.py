a4919af73c3f1fd40bb35e4725a83da6e2cd3a23
Luca Brüssow <210828907+lucabruessow@users.noreply.github.com>
2025-07-01 12:56:40 +0200

    feat: add route point storage on map click

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
-import gpxpy
 import streamlit as st
 import ollama
 import folium
 from streamlit_folium import st_folium
 
+st.session_state.setdefault('route_points', [])
 
-st.title("Smart routeplanner")
+st.title("KI Routenplaner")
 
-st.write("Hello World")
+
+st.subheader("Karte")
+
+# Karte erstellen
+karte = folium.Map(
+    location = [50.96282, 14.07066],
+    zoom_start = 18
+)
+
+# Normalen und Satellitenlayer hinzufügen
+folium.TileLayer('OpenStreetMap').add_to(karte)
+folium.TileLayer(
+    tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    attr = 'Esri',
+    name = 'Satellit',
+    overlay = False,
+    control = True
+    ).add_to(karte)
+
+folium.LayerControl().add_to(karte)
+
+# Karte anzeigen
+map_data = st_folium(karte, width=700, height=500)
+
+# Klickevent verarbeiten
+if map_data['last_clicked']:
+    st.write('Koordinaten: ', map_data['last_clicked']['lat'], map_data['last_clicked']['lng'])
+    st.session_state.route_points.append([map_data['last_clicked']['lat'], map_data['last_clicked']['lng']])
