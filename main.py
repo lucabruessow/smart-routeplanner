240ba79aa512b0eaa4666536c00d922b3a90fbc7
Luca Brüssow <210828907+lucabruessow@users.noreply.github.com>
2025-07-03 11:59:14 +0200

    refactor: improve marker creation

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ import folium
 from streamlit_folium import st_folium
 
 st.session_state.setdefault('route_points', [])
+st.session_state.setdefault('zoom', 18)
+st.session_state.setdefault('location', [50.96282, 14.07066])
 
 st.title("KI Routenplaner")
-
-
 st.subheader("Karte")
 
 # Karte erstellen
-karte = folium.Map(
-    location = [50.96282, 14.07066],
-    zoom_start = 18
+m = folium.Map(
+    location = st.session_state.location,
+    zoom_start = st.session_state.zoom
 )
 
 # Normalen und Satellitenlayer hinzufügen
-folium.TileLayer('OpenStreetMap').add_to(karte)
+folium.TileLayer('OpenStreetMap').add_to(m)
 folium.TileLayer(
     tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
     attr = 'Esri',
@@ -25,18 +25,27 @@ folium.TileLayer(
     show = False,
     overlay = True,
     control = True
-    ).add_to(karte)
+    ).add_to(m)
 
-folium.LayerControl().add_to(karte)
+folium.LayerControl().add_to(m)
 
 for i, coord in enumerate(st.session_state.route_points):
-    folium.Marker(coord, popup=f"{i+1}. Wegpunkt", tooltip=f"{i+1}. Wegpunkt").add_to(karte)
+    folium.Marker(
+        location=coord,
+        popup=f"{i+1}. Wegpunkt",
+        tooltip=f"{i+1}. Wegpunkt"
+    ).add_to(m)
 
-# Karte anzeigen
-map_data = st_folium(karte, width=700, height=500)
+# Karte rendern
+map = st_folium(m, width=700, height=500)
 
 # Klickevent verarbeiten
-if map_data['last_clicked']:
-    st.write('Koordinaten: ', map_data['last_clicked']['lat'], map_data['last_clicked']['lng'])
-    st.session_state.route_points.append([map_data['last_clicked']['lat'], map_data['last_clicked']['lng']])
+if map['last_clicked']:
+    lat, lng = map['last_clicked']['lat'], map['last_clicked']['lng']
+    st.write('Koordinaten: ', lat, lng)
 
+    # Neuen Punkt
+    st.session_state.route_points.append([lat, lng])
+    st.session_state.location = [map['center']['lat'], map['center']['lng']]
+    st.session_state.zoom = map['zoom']
+    st.rerun()
